7f40912956d8293f01d46e92dc8af73e0b1bdf92
Moe Burney <moeburney@gmail.com>
2013-01-25 20:52:39 +0100

    latest update

--- MailFilter/ImapSync.m
+++ MailFilter/ImapSync.m
@@ -22,29 +22,22 @@
     self = [super init];
     self.accountNum = 1;
     self.account = [[CTCoreAccount alloc] init];
-    /*
+        
     BOOL success = [self.account connectToServer:[Settings server:self.accountNum]
                                             port:[Settings serverPort:self.accountNum]
-                                  connectionType:CONNECTION_TYPE_PLAIN
-                                         authType:IMAP_AUTH_TYPE_PLAIN
-                                           login:[Settings username:self.accountNum]
-                                        password:[Settings password:self.accountNum]];
-    */
-    /*
-    BOOL success = [self.account connectToServer:[Settings server:self.accountNum]
-                                            port:[Settings serverPort:self.accountNum]
-                                  connectionType:CONNECTION_TYPE_PLAIN
+                                  connectionType:CONNECTION_TYPE_TLS
                                         authType:IMAP_AUTH_TYPE_PLAIN
                                            login:[Settings username:self.accountNum]
                                         password:[Settings password:self.accountNum]];
-     */
+     
+    /*
     BOOL success = [self.account connectToServer:@"localhost"
                                             port:10143
                                   connectionType:CONNECTION_TYPE_PLAIN
                                         authType:IMAP_AUTH_TYPE_PLAIN
                                            login:@"moeburney"
                                         password:@"password"];
-    
+    */
 
     if (!success)
     {
@@ -75,39 +68,11 @@
     CTCoreFolder *inbox = [self.account folderWithPath:@"INBOX"];
     NSArray *messages = [inbox messagesFromSequenceNumber:1 to:5 withFetchAttributes:CTFetchAttrEnvelope];
     NSMutableArray *message_array = [[NSMutableArray alloc] init];
-
-    NSMutableArray *messages_mutable = [[NSMutableArray alloc] initWithArray:messages];
-
     
-    // Manually remove old messages by looping through them all and checking if not new
-    // todo: find a more efficient way to do this
-    for (int i = 0; i < messages_mutable.count; i++)
-    {
-        if (![[messages_mutable objectAtIndex:i] isNew])
-        {
-            [messages_mutable removeObject:[messages_mutable objectAtIndex:i]];
-            //NSLog(@"found old message");
-        }
-    }
-    
-    NSLog(@"%i", messages_mutable.count);
-    
-    int msg_batch_total = 0;
-    
-    if (messages_mutable.count > 5)
-    {
-        msg_batch_total = 5;
-    }
-    else
-    {
-        msg_batch_total = messages_mutable.count;
-    }
-    
-    for (int i = 0; i < msg_batch_total; i++) {
-        NSLog(@"looping");
+    for (int i = 0; i < [messages count]; i++) {
         aMessage = [[Message alloc] init];
         aMessage.messageID = i;
-        CTCoreMessage *msg  = [messages_mutable objectAtIndex:i];
+        CTCoreMessage *msg  = [messages objectAtIndex:i];
         BOOL isHTML;
         aMessage.sequenceNumber = [msg sequenceNumber];
         aMessage.body = [msg bodyPreferringPlainText:&isHTML];
@@ -115,8 +80,10 @@
 
         aMessage.senderEmailAddress = [[msg sender] email];
         aMessage.senderName = [[msg sender] name];
-        NSLog(@"%@", aMessage.senderEmailAddress);
-        NSLog(@"%@", aMessage.senderName);
+        //NSLog(@"%@", aMessage.senderEmailAddress);
+        //NSLog(@"%@", aMessage.senderName);
+  
+
 
         
         aMessage.date = [msg senderDate];
@@ -140,37 +107,11 @@
     CTCoreFolder *inbox = [self.account folderWithPath:@"INBOX"];
     NSArray *messages = [inbox messagesFromSequenceNumber:start to:finish withFetchAttributes:CTFetchAttrEnvelope];
     NSMutableArray *message_array = [[NSMutableArray alloc] init];
-    
-    NSMutableArray *messages_mutable = [[NSMutableArray alloc] initWithArray:messages];
-
-    
-    // Manually remove old messages by looping through them all and checking if not new
-    // todo: find a more efficient way to do this
-    for (int i = 0; i < messages_mutable.count; i++)
-    {
-        if (![[messages_mutable objectAtIndex:i] isNew])
-        {
-            [messages_mutable removeObject:[messages_mutable objectAtIndex:i]];
-           // NSLog(@"found old message");
-        }
-    }
-    
-    
-    int msg_batch_total = 0;
-    
-    if (messages_mutable.count > 5)
-    {
-        msg_batch_total = 5;
-    }
-    else
-    {
-        msg_batch_total = messages_mutable.count;
-    }
-    
-    for (int i = 0; i < msg_batch_total; i++) {
+      
+    for (int i = 0; i < [messages count] - 1; i++) {
         aMessage = [[Message alloc] init];
         aMessage.messageID = i;
-        CTCoreMessage *msg  = [messages_mutable objectAtIndex:i];
+        CTCoreMessage *msg  = [messages objectAtIndex:i];
         BOOL isHTML;
         aMessage.sequenceNumber = [msg sequenceNumber];
         aMessage.body = [msg bodyPreferringPlainText:&isHTML];
